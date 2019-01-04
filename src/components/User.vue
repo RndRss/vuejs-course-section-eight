6ae2b41fe52d8f4dbaf5767d56dc991b3dad585c
ronaldorissardo <ronaldorissardo@gmail.com>
2019-01-04 21:28:32 -0200

    up

--- src/components/User.vue
+++ src/components/User.vue
@@ -2,10 +2,13 @@
     <div class="component">
         <h1>The User Component</h1>
         <p>I'm an awesome User!</p>
+        <button @click="changeName">Change my name</button>
+        <p>Name is {{ name }}</p>
+
         <hr>
         <div class="row">
             <div class="col-xs-12 col-sm-6">
-                <app-user-detail></app-user-detail>
+                <app-user-detail :myName="name" @nameWasReset="name = $event"></app-user-detail>
             </div>
             <div class="col-xs-12 col-sm-6">
                 <app-user-edit></app-user-edit>
@@ -19,6 +22,17 @@
     import UserEdit from './UserEdit.vue';
 
     export default {
+        data: function () {
+            return {
+                name: 'Name'
+            }
+
+        },
+        methods: {
+            changeName() {
+                this.name = 'Other Name'
+            }
+        },
         components: {
             appUserDetail: UserDetail,
             appUserEdit: UserEdit
--- src/components/UserDetail.vue
+++ src/components/UserDetail.vue
@@ -2,10 +2,30 @@
     <div class="component">
         <h3>You may view the User Details here</h3>
         <p>Many Details</p>
+        <p>User Name: {{ switchName() }}</p>
+        <button @click="resetName">Reset Nane</button>
     </div>
 </template>
 
 <script>
+    export default {
+        props: {
+            myName: {
+                type: String,
+                default: 'Name2'
+            }
+        },
+        methods: {
+            switchName() {
+                return this.myName.split("").reverse().join("");
+            },
+            resetName() {
+                this.myName = "NAMEEE";
+                this.$emit('nameWasReset', this.myName);
+
+            }
+        }
+    }
 </script>
 
 <style scoped>
