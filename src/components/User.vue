7e8bc204b8f97b7398a942dc0b8fd4502caac540
Ronaldo Rissardo <ronaldorissardo@gmail.com>
2019-01-05 13:07:56 -0200

    communication between components

--- src/components/User.vue
+++ src/components/User.vue
@@ -8,7 +8,7 @@
         <hr>
         <div class="row">
             <div class="col-xs-12 col-sm-6">
-                <app-user-detail :myName="name" @nameWasReset="name = $event"></app-user-detail>
+                <app-user-detail :myName="name" @nameWasReset="name = $event" :resetFn="resetName"></app-user-detail>
             </div>
             <div class="col-xs-12 col-sm-6">
                 <app-user-edit></app-user-edit>
@@ -30,7 +30,10 @@
         },
         methods: {
             changeName() {
-                this.name = 'Other Name'
+                this.name = 'Other Name';
+            },
+            resetName(){
+                this.name = 'Another one';
             }
         },
         components: {
--- src/components/UserDetail.vue
+++ src/components/UserDetail.vue
@@ -3,7 +3,9 @@
         <h3>You may view the User Details here</h3>
         <p>Many Details</p>
         <p>User Name: {{ switchName() }}</p>
-        <button @click="resetName">Reset Nane</button>
+        <button @click="resetName">Reset Name</button>
+        <button @click='resetFn()'>Reset Name</button>
+
     </div>
 </template>
 
@@ -13,7 +15,8 @@
             myName: {
                 type: String,
                 default: 'Name2'
-            }
+            },
+            resetFn: Function
         },
         methods: {
             switchName() {
